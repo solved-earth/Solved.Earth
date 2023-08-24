bad33c6cea6e67e5faea81d9dedb4959b8311d39
loupdaniel <loupdaniel12@gmail.com>
2023-08-25 03:04:03 +0900

    Connecting API (error occurred)

--- lib/resources/image_store_methods.dart
+++ lib/resources/image_store_methods.dart
@@ -51,14 +51,17 @@ class ImageStoreMethods {
 }
 */
 import 'dart:typed_data';
+import 'dart:convert';
 
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:uuid/uuid.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:app/models/post_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:app/components/certification_tile.dart';
+import 'package:http/http.dart' as http;
 
 class ImageStoreMethods {
   final FirebaseStorage _storage = FirebaseStorage.instance;
@@ -88,31 +91,53 @@ class ImageStoreMethods {
     return downloadUrl;
   }
 
+  //! Replaced the Future type from Future<String> to Future<Object>
   Future<String> uploadPost(
       String description, Uint8List file, int index) async {
     String res = 'Some Error Occurred';
+    String neterror = 'Network Error Occurred';
 
-    //int index = ref.watch(indexProvider);
-    try {
-      String photoUrl = await imageToStorage(file, index);
-      String postId = const Uuid().v1();
+    String apiUrl =
+        'http://49.247.29.177:8000/upload-images/?username=${currentUserUid}&challenge_name=${index.toString()}';
+    Uri uri = Uri.parse(apiUrl);
 
-      Post post = Post(
-        //description: description,
-        postId: postId,
-        datePublished: DateTime.now(),
-        postUrl: photoUrl,
-      );
-      _firestore
-          .collection('users/$currentUserUid/${index.toString()}/')
-          .doc(postId)
-          .set(
-            post.toJson(),
-          ); // ! currentUserUid 에서 문제 발생 시 index를 certification_tile 에서 받아온 방법 사용
-      res = 'success';
-    } catch (err) {
-      res = err.toString();
+    // Send the API request
+    var response = await http.post(uri, body: {
+      'image': file, //! Uint8List가 아니라 원본 사진을 보내야 할 수도
+    });
+
+    // Handle the API response
+    if (response.statusCode == 200) {
+      //int index = ref.watch(indexProvider);
+      var response = await http.get(uri);
+      Map<String, dynamic> responseBodyMap = jsonDecode(response.body);
+      if (responseBodyMap["success"] == true) {
+        try {
+          String photoUrl = await imageToStorage(file, index);
+          String postId = const Uuid().v1();
+
+          Post post = Post(
+            //description: description,
+            postId: postId,
+            datePublished: DateTime.now(),
+            postUrl: photoUrl,
+          );
+          _firestore
+              .collection('users/$currentUserUid/${index.toString()}/')
+              .doc(postId)
+              .set(
+                post.toJson(),
+              );
+          res = 'success';
+        } catch (err) {
+          res = err.toString();
+        }
+        return res;
+      } else {
+        return responseBodyMap["reason"]; // Returns missing values
+      }
+    } else {
+      return neterror;
     }
-    return res;
   }
 }
