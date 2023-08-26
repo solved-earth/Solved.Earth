6ff778bb11a5b2ee3b2b20afef8dd57f3a6f0732
jsj <120244130+jsjin7371@users.noreply.github.com>
2023-08-26 17:51:47 +0900

    solve err in api

--- lib/resources/image_store_methods.dart
+++ lib/resources/image_store_methods.dart
@@ -1,58 +1,7 @@
-/*
-import 'dart:typed_data';
-
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:uuid/uuid.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:app/models/post_model.dart';
-
-class ImageStoreMethods {
-  final FirebaseStorage _storage = FirebaseStorage.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  Future<String> imageToStorage(Uint8List file) async {
-    String id = const Uuid().v1();
-    // !! need to receive $userId and $challengeId
-    Reference ref = _storage
-        .ref()
-        .child('posts')
-        .child(id); // To specify the folder names for the post and post id
-    // !! _storage.ref().child("users/" + $userId + $challengeId + id);
-
-    // Reference is an object that represents a reference to a file or a directory in a cloud storage service such as firebase storage
-
-    UploadTask uploadTask = ref.putData(
-        file); // UploadTask is an object that represents a task for uploading a file to a cloud storage service such as firebase storage
-    TaskSnapshot snapshot = await uploadTask; // Can monitor the upload progress
-    String downloadUrl = await snapshot.ref.getDownloadURL();
-    return downloadUrl;
-  }
-
-  Future<String> uploadPost(String description, Uint8List file) async {
-    String res = 'Some Error Occurred';
-    try {
-      String photoUrl = await imageToStorage(file);
-      String postId = const Uuid().v1();
-      Post post = Post(
-        //description: description,
-        postId: postId,
-        datePublished: DateTime.now(),
-        postUrl: photoUrl,
-      );
-      _firestore.collection('posts').doc(postId).set(
-            post.toJson(),
-          );
-      res = 'success';
-    } catch (err) {
-      res = err.toString();
-    }
-    return res;
-  }
-}
-*/
 import 'dart:typed_data';
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 import 'package:uuid/uuid.dart';
@@ -61,6 +10,7 @@ import 'package:app/models/post_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:http_parser/src/media_type.dart';
 
 class ImageStoreMethods {
   final FirebaseStorage _storage = FirebaseStorage.instance;
@@ -97,32 +47,55 @@ class ImageStoreMethods {
     String neterror = 'Network Error Occurred';
 
     String apiUrl =
-        'http://49.247.29.177:8000/upload-images/?username=$currentUserUid&challenge_name=${index.toString()}';
+        'http://49.247.29.177:8000/upload-images?username=$currentUserUid&challenge_name=recycle';
     Uri uri = Uri.parse(apiUrl);
 
-    // Send the API request
+    // Send the API request in http
     /*var response = await http.post(uri, body: {
       'image': file, //! Uint8List가 아니라 원본 사진을 보내야 할 수도
     });*/
 
-    var request = http.MultipartRequest('POST', uri)
-      ..files.add(http.MultipartFile(
-        'image', // File field name
-        Stream.fromIterable(file.map(
-            (int byte) => [byte])), // Convert Uint8List to Stream<List<int>>
-        file.length, // Length of the Uint8List
-      ));
+    /*var request = http.MultipartRequest('POST', uri)
+      ..files.add(
+        http.MultipartFile(
+          'file', // File field name
+          Stream.fromIterable([file]),
+          file.length,
+          filename: 'file${index.toString()}.png',
+          contentType: MediaType('image', 'png'),
+        ),
+      );
+
+
 
     var response = await request.send();
-    print('응답 상태 코드: ${response.statusCode}');
-    print('응답 이유: ${response.reasonPhrase}');
-    print('응답 이유 2: ${response.isRedirect}');
-    print('응답 이유 3: ${response.headers}');
+    print('statusCode: ${response.statusCode}');
+    print('reasonPhrase: ${response.reasonPhrase}');
+    print('isRedirect: ${response.isRedirect}');
+    print('headers: ${response.headers}');
+    print('persistentConnection: ${response.persistentConnection}');
+    print('response.stream: ${await response.stream.bytesToString()}');*/
+
+    //send api request in dio
+    var dio = Dio();
+    var formData = FormData.fromMap({
+      'file':
+          MultipartFile.fromBytes(file, filename: 'file${index.toString()}.png')
+    });
+
+    var response = await dio.postUri(uri, data: formData);
+
+    print('isRedirect: ${response.isRedirect}');
+    print('statusCode: ${response.statusCode}');
+    print('reasonPhrase: ${response.statusMessage}');
+    print('headers: ${response.headers}');
+    print('persistentConnection: ${response.extra}');
+    print('data: ${response.data}');
 
     // Handle the API response
     if (response.statusCode == 200) {
       //int index = ref.watch(indexProvider);
-      var response = await http.get(uri);
+      //var response = await http.get(uri);
       Map<String, dynamic> responseBodyMap = jsonDecode(response.body);
       if (responseBodyMap["success"] == true) {
         try {
