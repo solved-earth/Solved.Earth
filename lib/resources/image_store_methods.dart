21a5e512deb7b8f1e3a869f49ee982d59aaf1a00
jsj <120244130+jsjin7371@users.noreply.github.com>
2023-08-27 21:37:23 +0900

    Added server response failure function

--- lib/resources/image_store_methods.dart
+++ lib/resources/image_store_methods.dart
@@ -1,5 +1,4 @@
 import 'dart:typed_data';
-import 'dart:convert';
 
 import 'package:dio/dio.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -8,15 +7,10 @@ import 'package:uuid/uuid.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:app/models/post_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:app/utils/utils.dart';
-
-import 'package:http/http.dart' as http;
-import 'package:http_parser/src/media_type.dart';
 
 class ImageStoreMethods {
   final FirebaseStorage _storage = FirebaseStorage.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  //int index = ref.watch(indexProvider);
 
   late int index;
 
@@ -51,81 +45,61 @@ class ImageStoreMethods {
         'http://49.247.29.177:8000/upload-images?username=$currentUserUid&challenge_name=recycle';
     Uri uri = Uri.parse(apiUrl);
 
-    // Send the API request in http
-    /*var response = await http.post(uri, body: {
-      'image': file, //! Uint8List가 아니라 원본 사진을 보내야 할 수도
-    });*/
+    // Handle the API response
+    Future<String> handleApiResponse(Response<dynamic> response) async {
+      if (response.statusCode == 200) {
+        Map<String, dynamic> responseBodyMap = response.data;
+        if (responseBodyMap["success"] == true) {
+          try {
+            String photoUrl = await imageToStorage(file, index);
+            String postId = const Uuid().v1();
 
-    /*var request = http.MultipartRequest('POST', uri)
-      ..files.add(
-        http.MultipartFile(
-          'file', // File field name
-          Stream.fromIterable([file]),
-          file.length,
-          filename: 'file${index.toString()}.png',
-          contentType: MediaType('image', 'png'),
-        ),
-      );
-
-
-
-    var response = await request.send();
-    print('statusCode: ${response.statusCode}');
-    print('reasonPhrase: ${response.reasonPhrase}');
-    print('isRedirect: ${response.isRedirect}');
-    print('headers: ${response.headers}');
-    print('persistentConnection: ${response.persistentConnection}');
-    print('response.stream: ${await response.stream.bytesToString()}');*/
+            Post post = Post(
+              //description: description,
+              postId: postId,
+              datePublished: DateTime.now(),
+              postUrl: photoUrl,
+            );
+            _firestore
+                .collection('users/$currentUserUid/${index.toString()}/')
+                .doc(postId)
+                .set(
+                  post.toJson(),
+                );
+            res = 'success';
+          } catch (err) {
+            res = err.toString();
+          }
+          return res;
+        } else {
+          res = 'Missing values: ${responseBodyMap["missing"].toString()}';
+          return res; // Returns missing values
+        }
+      } else {
+        return neterror;
+      }
+    }
 
     //send api request in dio
     var dio = Dio();
+    dio.options.connectTimeout = const Duration(
+        seconds:
+            5); //If the connect time from the server is longer than 5 seconds, return connectTimeout Exception
+    dio.options.receiveTimeout = const Duration(
+        seconds:
+            5); //If the receive time from the server is longer than 5 seconds, return receiveTimeout Exception
+
     var formData = FormData.fromMap({
       'file':
           MultipartFile.fromBytes(file, filename: 'file${index.toString()}.png')
     });
 
-    var response = await dio.postUri(uri, data: formData);
-
-    print('isRedirect: ${response.isRedirect}');
-    print('statusCode: ${response.statusCode}');
-    print('reasonPhrase: ${response.statusMessage}');
-    print('headers: ${response.headers}');
-    print('persistentConnection: ${response.extra}');
-    print('data: ${response.data}');
-
-    // Handle the API response
-    if (response.statusCode == 200) {
-      //int index = ref.watch(indexProvider);
-      //var response = await http.get(uri);
-      Map<String, dynamic> responseBodyMap = response.data;
-      if (responseBodyMap["success"] == true) {
-        try {
-          String photoUrl = await imageToStorage(file, index);
-          String postId = const Uuid().v1();
-
-          Post post = Post(
-            //description: description,
-            postId: postId,
-            datePublished: DateTime.now(),
-            postUrl: photoUrl,
-          );
-          _firestore
-              .collection('users/$currentUserUid/${index.toString()}/')
-              .doc(postId)
-              .set(
-                post.toJson(),
-              );
-          res = 'success';
-        } catch (err) {
-          res = err.toString();
-        }
-        return res;
-      } else {
-        res = 'Missing values: ${responseBodyMap["missing"].toString()}';
-        return res; // Returns missing values
-      }
-    } else {
-      return neterror;
+    try {
+      var response = await dio.postUri(uri, data: formData);
+      return handleApiResponse(response);
+    } catch (e) {
+      print(e);
+      return '서버와 통신 실패';
     }
   }
 }
